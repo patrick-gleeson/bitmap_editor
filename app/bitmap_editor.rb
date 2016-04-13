5e3fe5434c654a671ce00d3317cccecfd40e514b
Patrick <patrick.c.gleeson@googlemail.com>
2016-04-13 09:22:32 +0100

    Standardize style

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,5 +1,4 @@
 class BitmapEditor
-
   def run
     @running = true
     $stdout.puts 'type ? for help'
@@ -7,24 +6,25 @@ class BitmapEditor
       $stdout.print '> '
       input = $stdin.gets.chomp
       case input
-        when '?'
-          show_help
-        when 'X'
-          exit_console
-        else
-          $stdout.puts 'unrecognised command :('
+      when '?'
+        show_help
+      when 'X'
+        exit_console
+      else
+        $stdout.puts 'unrecognised command :('
       end
     end
   end
 
   private
-    def exit_console
-      $stdout.puts 'goodbye!'
-      @running = false
-    end
 
-    def show_help
-      $stdout.puts "? - Help
+  def exit_console
+    $stdout.puts 'goodbye!'
+    @running = false
+  end
+
+  def show_help
+    $stdout.puts "? - Help
 I M N - Create a new M x N image with all pixels coloured white (O).
 C - Clears the table, setting all pixels to white (O).
 L X Y C - Colours the pixel (X,Y) with colour C.
@@ -32,5 +32,5 @@ V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 an
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
 S - Show the contents of the current image
 X - Terminate the session"
-    end
+  end
 end
